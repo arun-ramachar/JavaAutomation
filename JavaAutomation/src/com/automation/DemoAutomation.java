c9edc7735df0461f087b082a864bc9c3ed684270
ArunRamachar <arunramachar@gmail.com>
2022-11-14 15:33:47 +0530

    pushing automation project

---
+++ JavaAutomation/src/com/automation/DemoAutomation.java
@@ -0,0 +1,18 @@
+package com.automation;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoAutomation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\AutomationDrivers\\chromedriver.exe");
+		ChromeDriver driver= new  ChromeDriver();
+		driver.get("http://www.google.com");
+		
+
+	}
+
+}
